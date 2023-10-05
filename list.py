ceb7dd3160c134eaefe63e1cefe0639789ef4836
FAYIZ OK <123459195+FAYIZO@users.noreply.github.com>
2023-10-05 16:06:59 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,11 @@
+a=input("\n list of integers\n")
+list1=[-1,-2,-3,-4,1,2,3,4]
+listp=[]
+for n in list1:
+    print(n,end="")
+    print("\n +ve integes are \n")
+    for n in list1:
+        if n>0:
+            listp.append(n)
+            for n in listp:
+                print(n,end="")
